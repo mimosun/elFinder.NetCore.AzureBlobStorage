afe2d911395bbe3a26acffd06c07f15f305f2ccd
ThaiTD <trieuducthai@gmail.com>
2023-12-15 14:01:10 +0700

    Add elFinder.NetCore package and fix secure

---
+++ src/elFinder.NetCore/Helpers/PathHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace elFinder.NetCore.Helpers
+{
+    public class PathHelper
+    {
+        private static readonly char[] SeparatorChars = new[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar };
+
+        public static string GetFullPath(string path)
+        {
+            var fullPath = Path.GetFullPath(path);
+            return fullPath;
+        }
+
+        public static string GetFullPathNormalized(string path)
+        {
+            var fullPath = Path.GetFullPath(path).TrimEnd(SeparatorChars);
+            return fullPath;
+        }
+
+        public static string NormalizePath(string fullPath)
+        {
+            return fullPath.TrimEnd(SeparatorChars);
+        }
+
+        public static string SafelyCombine(string fromParent, params string[] paths)
+        {
+            var finalPath = Path.GetFullPath(Path.Combine(paths))
+                .Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+
+            if (!finalPath.StartsWith(fromParent.TrimEnd(SeparatorChars) + Path.DirectorySeparatorChar))
+                throw new ArgumentException("Path must be inside parent");
+
+            return finalPath;
+        }
+    }
+}
